10afe4df249dbeb70da64a0a95c699b90a8aa51d
albarto2021 <81423967+albarto2021@users.noreply.github.com>
2021-07-28 23:59:03 +0300

    Initial commit

---
+++ src/main/java/com/bank/payload/response/UserResponse.java
@@ -0,0 +1,11 @@
+package com.bank.payload.response;
+
+import com.bank.dao.UserDAO;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserResponse {
+    List<UserDAO> users;
+}
---
+++ src/main/java/com/bank/service/TransactionService.java
@@ -0,0 +1,7 @@
+package com.bank.service;
+
+import com.bank.model.Transaction;
+
+public interface TransactionService {
+    void saveTransaction(Transaction transaction);
+}
